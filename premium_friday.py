0f8449104d60e1abf3a0e08474a9850f5f9a0c82
hebinotoshi <alegaglio@gmail.com>
2023-03-24 13:14:17 +0900

    Update premium_friday.py

--- premium_friday.py
+++ premium_friday.py
@@ -1,10 +1,18 @@
 from datetime import date, timedelta
 
-def is_last_friday():
-    today = date.today()
-    last_day_of_month = today.replace(day=28) + timedelta(days=4)  # add 4 days to get to the last day of the month
-    if today.month != last_day_of_month.month:
-        last_day_of_month = last_day_of_month.replace(day=1)  # if the last day is in the next month, set it to the first day of the next month
-    days_until_last_friday = (last_day_of_month - today).days
-    return days_until_last_friday == 0 and today.weekday() == 4  # 4 is Friday
+def is_premium_friday(day):
+    final = (day.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
+    return day.weekday() == 4 and final - day < timedelta(days=7)
 
+def is_premiumer_friday(day):
+    return is_premium_friday(day) and day.month % 3 == 0
+
+def is_premiumest_friday(day):
+    return is_premium_friday(day) and day.month == 12
+
+today = date.today()
+print(f"Today ({today}) is Premium Friday? {is_premium_friday(today)}")
+print(f"March 31, 2023 is Premium Friday? {is_premium_friday(date(2023, 3, 31))}")
+print(f"March 31, 2023 is Premiumer Friday? {is_premiumer_friday(date(2023, 3, 31))}")
+print(f"December 29, 2023 is Premium Friday? {is_premium_friday(date(2023, 12, 29))}")
+print(f"December 29, 2023 is Premiumest Friday? {is_premiumest_friday(date(2023, 12, 29))}")

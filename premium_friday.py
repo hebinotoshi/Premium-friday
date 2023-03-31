417664ec4f2cc925411c9cdb1df125c5318e4b70
hebinotoshi <alegaglio@gmail.com>
2023-03-31 09:01:05 +0900

    Update premium_friday.py

--- premium_friday.py
+++ premium_friday.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 
 def is_premium_friday(day):
     final = (day.replace(day=28) + timedelta(days=4)).replace(day=1) - timedelta(days=1)
@@ -10,9 +10,26 @@ def is_premiumer_friday(day):
 def is_premiumest_friday(day):
     return is_premium_friday(day) and day.month == 12
 
-today = date.today()
-print(f"Today ({today}) is Premium Friday? {is_premium_friday(today)}")
-print(f"March 31, 2023 is Premium Friday? {is_premium_friday(date(2023, 3, 31))}")
-print(f"March 31, 2023 is Premiumer Friday? {is_premiumer_friday(date(2023, 3, 31))}")
-print(f"December 29, 2023 is Premium Friday? {is_premium_friday(date(2023, 12, 29))}")
-print(f"December 29, 2023 is Premiumest Friday? {is_premiumest_friday(date(2023, 12, 29))}")
+def is_all_premiums(day):
+    return all(func(day) for func in [is_premium_friday, is_premiumer_friday, is_premiumest_friday])
+
+today = datetime.today()
+input_date = input("Enter a date (YYYY-MM-DD), or press Enter to use today's date: ")
+
+if input_date == '':
+    day = today
+else:
+    day = datetime.strptime(input_date, "%Y-%m-%d")
+
+if is_all_premiums(day):
+    print("This date is Premium, Premiumer, and Premiumest Friday")
+elif is_premium_friday(day) and is_premiumer_friday(day):
+    print("This date is both Premium and Premiumer Friday")
+elif is_premium_friday(day) and is_premiumest_friday(day):
+    print("This date is both Premium and Premiumest Friday")
+elif is_premiumer_friday(day) and is_premiumest_friday(day):
+    print("This date is both Premiumer and Premiumest Friday")
+elif is_premium_friday(day):
+    print("This date is Premium Friday")
+else:
+    print("This date is not a Premium Friday")
